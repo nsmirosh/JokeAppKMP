d05d4516248916a77e80e7a205b23a31df46dec2
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2024-12-16 20:24:25 +0700

    Some more cleanup

--- composeApp/src/commonMain/kotlin/nick/mirosh/joke/app/App.kt
+++ composeApp/src/commonMain/kotlin/nick/mirosh/joke/app/App.kt
@@ -2,14 +2,21 @@ package nick.mirosh.joke.app
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -17,6 +24,7 @@ import io.ktor.client.request.get
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
@@ -28,33 +36,46 @@ const val URL = "https://v2.jokeapi.dev/joke/Any?type=single"
 @Composable
 fun App() {
     MaterialTheme {
-        var client: HttpClient? = null
+        var client: HttpClient? by remember { mutableStateOf(null) }
         //to make sure that we're not creating a new client on every recomposition
+        var joke by remember { mutableStateOf<Joke?>(null) }
+
         LaunchedEffect(Unit) {
-            client = createPlatformHttpClient().config {
-                install(ContentNegotiation) {
-                    json(Json {
-                        ignoreUnknownKeys = true
-                        isLenient = true
-                    })
-                }
-            }
+            client = createHttpClient()
         }
         val scope = rememberCoroutineScope()
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Column {
-                Button(onClick = {
-                    scope.launch(Dispatchers.IO) {
-                        val joke: Joke = client!!.get(URL).body()
-                        println(joke.joke)
-                    }
-                }) {
-                    Text("Generate a joke")
+        Column(
+            Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = {
+                scope.launch(Dispatchers.IO) {
+                    joke = client!!.get(URL).body()
                 }
+            }) {
+                Text("Generate a joke")
+            }
+            joke?.let {
+                Text(
+                    modifier = Modifier.padding(16.dp),
+                    fontSize = 24.sp,
+                    text = it.joke
+                )
             }
         }
     }
 }
+
+private fun createHttpClient() =
+    createPlatformHttpClient().config {
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+                isLenient = true
+            })
+        }
+    }
+
 
 @Serializable
 data class Joke(
